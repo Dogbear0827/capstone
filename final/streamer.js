5c0076f7af8b991c0ffd5d712b4ed9346c99da99
Dogbear0827 <88106518+Dogbear0827@users.noreply.github.com>
2024-12-08 15:28:47 +0800

    Update streamer.js

--- final/streamer.js
+++ final/streamer.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket('wss://localhost:8080'); // 連接到 WebSocket 伺服器
+const socket = new WebSocket('wss://' + window.location.hostname + ':8080'); // 連接到 WebSocket 伺服器
 
 let name; // 用戶名稱
 let peerConnection; // WebRTC peer connection
