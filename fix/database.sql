91ce8b6fac2b4a1b0c87abe4f33ade6a1e90ee62
Kuanhsiaotse <aaa49722180@gmail.com>
2024-12-08 09:59:13 +0800

    Update database.sql

--- fix/database.sql
+++ fix/database.sql
@@ -11,6 +11,12 @@ CREATE TABLE users (
     updated_at DATETIME DEFAULT NOW() ,
     is_active TINYINT(1) DEFAULT 1
 );
+CREATE TABLE chat_messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username NVARCHAR(50) NOT NULL,
+    message TEXT NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+);
 
 CREATE INDEX idx_users_email ON users (email);
 CREATE INDEX idx_users_username ON users (username);
