33d56f8724c1cf4959db0ca2f70f747e75925618
Dogbear0827 <88106518+Dogbear0827@users.noreply.github.com>
2024-12-08 14:36:38 +0800

    Update streamer.js

--- final/streamer.js
+++ final/streamer.js
@@ -14,7 +14,7 @@ const stopButton = document.getElementById('stopButton');
 fetch('check-login.php')
     .then(response => response.json())
     .then(data => {
-        if (data.success) {
+        if (data.status === "success") {
             name = data.username; // 設置用戶名稱
             console.log(`用戶名稱: ${name}`);
 
